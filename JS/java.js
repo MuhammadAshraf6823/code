69aad80eeb2e9f4501af1b7432c2e4e901a604ec
MuhammadAshraf6823 <110913405+MuhammadAshraf6823@users.noreply.github.com>
2022-09-21 10:28:39 +0500

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,24 @@
+function change(){
+
+let fnum = document.getElementById("fnumber").value;
+let snum = document.getElementById("snumber").value;
+let fsnum = parseInt(fnum) + parseInt(snum)
+document.write(fsnum)
+}
+
+function change1(){
+
+    let fnum = document.getElementById("fnumber").value;
+    let snum = document.getElementById("snumber").value;
+    let fsnum = parseInt(fnum) - parseInt(snum)
+    document.write(fsnum)
+    }
+
+    function change2(){
+
+        let fnum = document.getElementById("fnumber").value;
+        let snum = document.getElementById("snumber").value;
+        let fsnum = parseInt(fnum) * parseInt(snum)
+        document.write(fsnum)
+        }
+    
