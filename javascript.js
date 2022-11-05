ab71ab95edbcfd488278ff6da56d0041f3e70745
MuhammadAshraf6823 <110913405+MuhammadAshraf6823@users.noreply.github.com>
2022-11-05 08:36:25 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function signup(){
+    let name = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    localStorage.setItem("name" , name);
+    localStorage.setItem("email" , email);
+    localStorage.setItem("password" , password);
+    }
+    
+    function login(){
+        console.log("login")
+       let loginemail = document.getElementById("email1").value;
+       let loginpassword = document.getElementById("password1").value;
+    
+        if (loginemail == localStorage.getItem("email") && loginpassword == localStorage.getItem("password")){
+            document.write("Password Correct Welcome   " + localStorage.getItem("name"))
+        }
+        else{ 
+            document.getElementById("message1").innerHTML="**Password Wrong**";
+        return false;
+    }
+    
+    }
